609daba7a4f7628a85676a52f1e8d77adc2e4eeb
zevilz <zacharygriffee@gmail.com>
2024-10-15 19:31:52 -0400

    add eventemitter poly

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import "./lib/util/setImmediatePoly.js";
+import "./lib/util/eventemitter.js";
 export * from "./lib/random-access-layered-storage.js";
 export * from "./lib/strict-random-access-layered-storage.js";
